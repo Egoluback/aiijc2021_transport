a14716e5856c859ab30b684e22363acc35bbff72
Egoluback <g.kokush5@gmail.com>
2021-08-14 16:32:30 +0300

    added and fixed tracks model

--- Model.py
+++ Model.py
@@ -2,10 +2,14 @@ import numpy as np
 import pandas as pd
 
 from collections import Counter
+from tqdm import tqdm
 
-from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LogisticRegression
 from catboost import CatBoostClassifier
+from sktime.transformations.panel.rocket import MiniRocket
+from sklearn.linear_model import RidgeClassifierCV
+
+from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_validate
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
@@ -17,7 +21,13 @@ warnings.filterwarnings("ignore")
 class Model:
     def __init__(self):        
         self.model = None
+        self.model_tracks = None
+        self.model_rocket = None
+
         self.counter_words = {}
+
+        self.TRACKS_CHUNK_SIZE = 20
+        self.TRACKS_MULTIPLIER = 1
     
     def count_words(self, x):
         return len(x.split(" "))
@@ -110,7 +120,7 @@ class Model:
     def estimate(self, X, y):
         return roc_auc_score(y, self.predict_proba(X, add_feat=False))
     
-    def train_test_split_(self, X, y, test_size, X_ss=None, y_ss=None, random_state=42):
+    def train_test_split_(self, X, y, test_size, X_ss=None, y_ss=None, random_state=RANDOM_STATE):
         if (X_ss is not None):
             X_ss_full, y_ss_full = self.label_shuffle(X, y, X_ss, y_ss, random_state = random_state)
             
@@ -139,7 +149,7 @@ class Model:
         
         return (X[: len_train], X[len_train :], y[: len_train], y[len_train :])
     
-    def train(self, X_train, X_test, y_train, y_test, categorical_features):
+    def train(self, X_train, X_test, y_train, y_test, categorical_feature, random_state=RANDOM_STATE):
         print(f"Train size: {X_train.shape}")
         print(f"Test size: {X_test.shape}")
         self.model = CatBoostClassifier(iterations=2000,
@@ -147,13 +157,13 @@ class Model:
                            silent=True,
                            loss_function='Logloss',
                            class_weights=(1, 2),
-                           random_state=42)
+                           random_state=random_state)
 
         self.model.fit(X_train, y_train, cat_features=categorical_features)
         
         return self.estimate(X_test, y_test)
     
-    def label_shuffle(self, X, y, X_ss, y_ss, random_state=42):
+    def label_shuffle(self, X, y, X_ss, y_ss, random_state=RANDOM_STATE):
         X_ss['ss'] = 1
         y_ss = y_ss.to_frame()
         y_ss['ss'] = 1
@@ -166,7 +176,7 @@ class Model:
         
         return (X_ss_full, y_ss_full)
     
-    def train_cross_validation(self, X, y, k, categorical_features, X_ss=None, y_ss=None, random_state=42):
+    def train_cross_validation(self, X, y, k, categorical_features, X_ss=None, y_ss=None, random_state=RANDOM_STATE):
         chunk_size = len(X) / k
         chunks_size = [(i*chunk_size, i*chunk_size + chunk_size) for i in range(k)]
         
@@ -215,7 +225,7 @@ class Model:
         
         return result_score
     
-    def fit_ss(self, X, y, numeric_features, categorial_features, X_ss, y_ss, cross_validation=False):
+    def fit_ss(self, X, y, numeric_features, categorial_features, X_ss, y_ss, cross_validation=False, random_state=RANDOM_STATE):
         self.counter_words = {}
         
         X_ = X
@@ -227,13 +237,13 @@ class Model:
         X_ss = self.add_features(X_ss)[numeric_features + categorical_features]
         
         if (not cross_validation):
-            X_train, X_test, y_train, y_test = self.train_test_split_(X_, y_, test_size=0.2, X_ss=X_ss, y_ss=y_ss, random_state=42)
+            X_train, X_test, y_train, y_test = self.train_test_split_(X_, y_, test_size=0.2, X_ss=X_ss, y_ss=y_ss, random_state=random_state)
             return self.train(X_train, X_test, y_train, y_test, categorical_features)
         else:
-            return self.train_cross_validation(X_, y_, 5, categorical_features, X_ss=X_ss, y_ss=y_ss, random_state=42)
+            return self.train_cross_validation(X_, y_, 5, categorical_features, X_ss=X_ss, y_ss=y_ss, random_state=random_state)
         
         
-    def fit(self, X, y, numeric_features, categorial_features, cross_validation=False):
+    def fit(self, X, y, numeric_features, categorial_features, cross_validation=False, random_state=RANDOM_STATE):
         self.counter_words = {}
         
         X_ = X
@@ -243,18 +253,50 @@ class Model:
         X_ = self.add_features(X_)[numeric_features + categorical_features]
 
         if (not cross_validation):
-            X_train, X_test, y_train, y_test = self.train_test_split_(X_, y_, test_size=0.2, random_state=42)
+            X_train, X_test, y_train, y_test = self.train_test_split_(X_, y_, test_size=0.2, random_state=random_state)
             return self.train(X_train, X_test, y_train, y_test, categorical_features)
         else:
-            return self.train_cross_validation(X_, y_, 5, categorical_features, random_state=42)
+            return self.train_cross_validation(X_, y_, 5, categorical_features, random_state=random_state)
+
+    def fit_tracks(self, tracks, random_state=RANDOM_STATE):
+        print("Preprocessing tracks data...")
+        X, y = self.tracks_preprocess(tracks, self.TRACKS_CHUNK_SIZE, self.TRACKS_MULTIPLIER)
+
+        print("Training MiniRocket...")
+        self.model_rocket = MiniRocket()
+        self.model_rocket.fit(X, y)
+
+        print("MiniRocket Transforming...")
+        X_train_transform = self.model_rocket.transform(X, y)
+
+        print("Training logistic regression...")
+        self.model_tracks = RidgeClassifierCV(normalize = True)
+        self.model_tracks.fit(X_train_transform, y)
         
+        print(f"Score: {self.model_tracks.score(X_train_transform, y)}")
     
+    def predict_tracks(self, tracks):
+        print("Preprocessing tracks data...")
+        X = self.tracks_preprocess(tracks, self.TRACKS_CHUNK_SIZE, self.TRACKS_MULTIPLIER, labled=False)
+        
+        print("MiniRocket Transforming...")
+        X_transform = self.model_rocket.transform(X)
+
+        return self.model_tracks.predict(X_transform)
+
     def predict_proba(self, X, add_feat=True):
         if (add_feat): X = self.add_features(X)
         
         X = X[numeric_features + categorical_features]
         
         return self.model.predict_proba(X).T[1]
+    
+    def predict(self, X, add_feat=True):
+        if (add_feat): X = self.add_features(X)
+        
+        X = X[numeric_features + categorical_features]
+        
+        return self.model.predict(X)
     
     def predict_thresh(self, X, thresh_above, thresh_below):
         y_unlab_full = self.predict_proba(X)
@@ -268,3 +310,58 @@ class Model:
         y_unlab.iloc[np.where(y_unlab_full <= thresh_below)] = 0
         
         return y_unlab
+
+    # undersampling method deletes some extra non aggressive values
+    def undersampling(self, X, multiplier):
+        aggressive_count = sum(X.is_aggressive==1)
+        non_aggressive_ind = X[X.is_aggressive==0].index
+
+        # number of aggressive and non-aggressive labels is the same
+        random_indices = np.random.choice(non_aggressive_ind, int(aggressive_count*multiplier), replace=False)
+        return pd.concat([X.loc[random_indices], X[X.is_aggressive==1]])
+
+    def split(self, arr, chunk_size = 15):
+        result = []
+        #get right length of arr so that it equally splits into chunks
+        length = len(arr)
+        split_length = length - (length%chunk_size)
+                
+        for i in range(split_length)[chunk_size::chunk_size]:
+            result.append(arr[i-chunk_size:i])
+
+        return np.array(result)
+
+    # make df, so that each row has whole order speeds time series
+    def make_nested(self, tracks, chunk_size, multiplier, labled):
+        unique_orders = tracks.drop_duplicates('order_id', keep='last')
+        if labled:
+            unique_orders = self.undersampling(unique_orders, multiplier)
+        y_labels = []
+        X_train = []
+        for order in tqdm(unique_orders['order_id']):
+            order_df = tracks[tracks.order_id == order]
+            order_df.loc[0, 'speed'] = 0
+
+            if order_df.shape[0] < chunk_size:
+                continue
+
+            splitted_arrs = self.split(order_df.values, chunk_size)
+            for arr in splitted_arrs:
+                if labled:
+                    is_aggressive = arr[0][6]
+                    y_labels.append(is_aggressive)
+                speed_series = []
+                for row in arr:  
+                    # append only speed and dt values 
+                    speed_series.append(row[5])
+                X_train.append(pd.Series(speed_series))
+        return X_train, y_labels
+    
+    def tracks_preprocess(self, tracks, chunk_size, multiplier, labled=True):
+        X_train, train_labels = self.make_nested(tracks, chunk_size, multiplier, labled)
+
+        X_train = pd.DataFrame({'speed': X_train})
+        if not labled: return X_train
+        y_train = np.array(train_labels)
+
+        return X_train, y_train
